d94af73dead8225e2f5bc7f614878a783d4ac8d0
Fedy chihaoui <sasouciouchiha@gmail.com>
2023-01-29 23:03:39 +0100

    returns its product with 'multiplier'."

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" returns its product with 'multiplier """
+
+from typing import Callable
+
+def create_multiplier(multiplier: float) -> Callable[[float], float]:
+    def multiply_by_multiplier(n: float) -> float:
+        return n * multiplier
+    return multiply_by_multiplier

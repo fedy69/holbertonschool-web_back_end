d31872e3c569c357f697934c3b9bfb16579e1163
Fedy chihaoui <sasouciouchiha@gmail.com>
2023-01-29 23:52:58 +0100

    fix

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,4 +6,4 @@ from typing import Callable
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     def multiply_by(n: float) -> float:
         return n * multiplier
-    return multiply_by_multiplier
+    return multiply_by

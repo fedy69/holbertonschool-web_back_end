d2836eb94bec977ecdd6da7783dc1778e0b34c84
Fedy chihaoui <sasouciouchiha@gmail.com>
2023-03-09 23:22:15 +0100

    lists all bands with Glam rock as their main style

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- lists all bands with Glam rock as their main style
 -- ranked by their longevity
 
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan FROM metal_bands 
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands
 WHERE style LIKE '%Glam rock%' ;

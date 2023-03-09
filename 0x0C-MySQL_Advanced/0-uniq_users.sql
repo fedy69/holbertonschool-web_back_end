808ff65cacac241f1c3fb887811c631c290ac3f8
Fedy chihaoui <sasouciouchiha@gmail.com>
2023-03-09 22:54:58 +0100

    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 --If the table already exists, your script should not fail
 
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255)
 );

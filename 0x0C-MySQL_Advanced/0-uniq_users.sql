aecda50dd8f99a77a3025ba83086efa3e48a9976
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2023-03-09 23:38:57 +0100

    script that creates a table users

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,10 @@
---  SQL script that creates a table users
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+--  If the table already exists, your script should not fail
+--  Your script can be executed on any database
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
-);
+)
